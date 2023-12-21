2e406e8c7fcfc985e0018c27f86a376de5661311
Keshav Kushwaha <keshavkushawaha@gmail.com>
2023-12-22 00:06:42 +0530

    Case check code updated

--- Upper Lower Numeric Identification/code.cpp
+++ Upper Lower Numeric Identification/code.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 
 int main () {
-    int value;
     cout << "Enter a character:"<< endl;
-
+    char value;
     cin >> value;
-    cout << "the value is:" << value << endl;
+    if (value>='a' && value<='z') {
+        cout<< "The character is lower case." << endl;
+    } else if (value>='A' && value<='Z') {
+        cout<< "The character is Upper case." << endl;
+    } else if(value>='1' && value<='9' ){
+        cout<< "The character is Number" << endl;
+    } else {
+        cout<< "The character is something else" << endl;
+    }
+
+
 }
