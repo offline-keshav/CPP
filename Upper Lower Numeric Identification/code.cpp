9f063923ac23b4c32379086eb8b9071474e0784e
Keshav Kushwaha <keshavkushawaha@gmail.com>
2023-12-21 13:28:44 +0530

    char identification

---
+++ Upper Lower Numeric Identification/code.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int value;
+    cout << "Enter a character:"<< endl;
+
+    cin >> value;
+    cout << "the value is:" << value << endl;
+}
