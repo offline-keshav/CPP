fbfa09b8aaf3b3e0f259ea2c078d4df6bd045af6
Keshav Kushwaha <keshavkushawaha@gmail.com>
2023-12-24 00:34:12 +0530

    Auto commit on 24-12-2023 12:34:12 AM

--- Pattern 2/git-update.cpp
+++ Pattern 2/git-update.cpp
@@ -4,7 +4,6 @@
 #include <io.h>
 using namespace std;
 
-// Function to get the current date and time as a string
 string getCurrentDateTime() {
     time_t now = time(0);
     struct tm *timeinfo;
@@ -17,30 +16,25 @@ string getCurrentDateTime() {
 }
 
 int main() {
-    // Execute the git add command
     if (system("git add .") != 0) {
         perror("Error running 'git add'");
         return 1;
     }
 
-    // Form the commit message with the current date and time
-    std::string commitMessage = "Auto commit on " + getCurrentDateTime();
+    string commitMessage = "Auto commit on " + getCurrentDateTime();
 
-    // Execute the git commit command
     string gitCommitCommand = "git commit -m \"" + commitMessage + "\"";
     if (system(gitCommitCommand.c_str()) != 0) {
         perror("Error running 'git commit'");
         return 1;
     }
 
-    // Execute the git push command
     if (system("git push") != 0) {
         perror("Error running 'git push'");
         return 1;
     }
 
-    // Success message
-    std::cout << "Git add, commit, and push commands executed successfully." << endl;
+    cout << "Git add, commit, and push commands executed successfully." << endl;
 
     return 0;
 }
