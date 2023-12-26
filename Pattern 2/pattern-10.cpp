67afa98a47a5858b34f32c864c565f514e155014
Keshav Kushwaha <keshavkushawaha@gmail.com>
2023-12-26 08:10:52 +0530

    Auto commit on 26-12-2023 08:10:52 AM

---
+++ Pattern 2/pattern-10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter number of rows: ";
+    cin>>n;
+    int row = 1;
+    char ch = 'A';
+    while (row<=n) {
+        int col = 1;
+        while (col<=row) {
+            cout<<ch<<" ";
+            col++;
+            ch++;
+        }
+        row++;
+        cout<<"\n";
+    }
+}
